4897601d0051029b5b4c5f685ed8cd16a0ddb7d8
Himanshu Maurya <himanshumaurya2214225@hotmail.com>
2017-03-15 17:43:42 +0530

    Added time.sleep() for task in queue to actually wait to be processed

--- queueProcess.py
+++ queueProcess.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 from PriorityQueue.HeapPriorityQueue import HeapPriorityQueue
 from Parsing.CSVInputParser import InputParser
@@ -19,12 +20,14 @@ def main(path_to_csv, starttime):
     while heapq:
         date_time, event = heapq.remove_min()
         date = date_time[0].strftime('%Y/%m/%d')
-        time = date_time[0].strftime('%H:%M')
-        date_time_str = ' '.join([str(date), str(time)])
+        time_ = date_time[0].strftime('%H:%M')
+        date_time_str = ' '.join([str(date), str(time_)])
+        seconds_difference = (date_time[0] - prev_time).total_seconds()
+        minute_difference = int(seconds_difference//60)
+        time.sleep(seconds_difference)
         if prev_time == date_time[0]:
             print("Current time [ {0} ] , Event \"{1}\" Processed".format(date_time_str, event))
         else:
-            minute_difference = int((date_time[0] - prev_time).total_seconds()//60)
             if first_time:
                 print("-- After {} minute --".format(str(minute_difference)))
                 first_time = False
